9a75efb3ed19558bd0a6a3cfd5d4edd5c10a26fd
Xiraeth <77112995+Xiraeth@users.noreply.github.com>
2023-10-16 12:06:07 +0300

    Completely revamp the project and how it is implemented
    Create functions for every action
    
    Make it so that drawing only happens when the game is clicked, not hovered
    
    Add black/colorful option to the screen
    
    Change 'user-select' to none so that boxes are not draggable

--- script.js
+++ script.js
@@ -1,77 +1,154 @@
-'use strict';
+"use strict";
 
-const container = document.querySelector('.container');
-const resetColorsBtn = document.querySelector('.remove');
-const changeSizeBtn = document.querySelector('.prompt');
+const container = document.querySelector(".container");
+const resetBtn = document.querySelector(".remove");
+const buttonsCategory = document.querySelector(".buttons");
+const buttons = document.querySelectorAll(".buttons button");
 const containerDimensions = 700;
 
-const rangeContainer = document.querySelector('.rangerContainer');
-const rangeValue = document.querySelector('.rangeValue');
-const rangeInput = document.querySelector('.rangeInput');
+const rangeContainer = document.querySelector(".rangerContainer");
+const rangeValue = document.querySelector(".rangeValue");
+const rangeInput = document.querySelector(".rangeInput");
 
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+let mode = "random rgb";
 let smallDivs;
-let side = 5;
+let side = rangeInput.value;
+
+pageLoad();
 
 function randomColorRGB() {
   return Math.floor(Math.random() * 255);
 }
 
-function addEventListeners(divs, btn) {
-  divs.forEach(div => div.addEventListener('mouseenter', () => {
+function swapActiveButton(btn) {
+  if (!btn) return;
+
+  buttons.forEach((button) => {
+    if (btn.classList.contains("remove")) return;
+    button.classList.remove("activeButton");
+  });
+  if (!btn.classList.contains("no-active")) btn.classList.add("activeButton");
+}
+
+function swapMode(btn) {
+  if (!btn) return;
+
+  if (btn.classList.contains("no-active")) return;
+  mode = btn.textContent.toLowerCase();
+}
+
+function changeRangeText() {
+  rangeValue.textContent = rangeInput.value;
+}
+
+function createGrid() {
+  const side = rangeInput.value;
+  container.innerHTML = "";
+
+  for (let i = 0; i < side * side; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `${containerDimensions / rangeInput.value}px`;
+    square.style.height = `${containerDimensions / rangeInput.value}px`;
+    square.addEventListener("mouseover", draw);
+    square.addEventListener("mousedown", draw);
+    container.appendChild(square);
+  }
+}
+
+function clearGrid() {
+  container.innerHTML = "";
+  createGrid();
+}
+
+function draw(e) {
+  const square = e.target;
+  if (e.type == "mouseover" && !mouseDown) return;
+
+  if (!square.classList.contains("square")) return;
+
+  if (mode == "random rgb") {
+    square.style.backgroundColor = `rgb(${randomColorRGB()},${randomColorRGB()},${randomColorRGB()})`;
+  } else if (mode == "black") {
+    square.style.backgroundColor = `black`;
+  }
+}
+
+function pageLoad() {
+  createGrid();
+  changeRangeText();
+}
+
+container.addEventListener("mousedown", function (e) {
+  draw(e);
+});
+resetBtn.addEventListener("click", clearGrid);
+rangeInput.addEventListener("input", () => {
+  changeRangeText();
+  createGrid();
+});
+buttonsCategory.addEventListener("click", (e) => {
+  swapActiveButton(e.target.closest("button"));
+  swapMode(e.target.closest("button"));
+});
+
+/*
+function applyColor(e) {
+  if (e.type == "mouseover" && !mouseDown) return;
+  if (mode == "rgb")
     div.style.backgroundColor = `rgb(${randomColorRGB()}, ${randomColorRGB()}, ${randomColorRGB()})`;
-  }))
-  
-  btn.addEventListener('click', e => {
-    divs.forEach(div => div.style.backgroundColor = 'rgba(0,0,0, 0.5)');
-  })
+  else if (mode == "black") div.style.backgroundColor = `0, 0, 0`;
+}
+
+function addEventListeners(divs, btn) {
+  divs.forEach((div) =>
+    // div.addEventListener("mouseenter", () => {
+    //   div.style.backgroundColor = `rgb(${randomColorRGB()}, ${randomColorRGB()}, ${randomColorRGB()})`;
+    // })
+    div.addEventListener("mouseover", applyColor)
+  );
+
+  btn.addEventListener("click", (e) => {
+    divs.forEach((div) => (div.style.backgroundColor = "rgba(0,0,0, 0.5)"));
+  });
 }
 
 function createDiv(divsPerSide, con) {
-  for(let i = 0; i < divsPerSide ** 2; i++) {
-    let newDiv = document.createElement('div');
+  for (let i = 0; i < divsPerSide ** 2; i++) {
+    let newDiv = document.createElement("div");
     newDiv.classList.add(`smallDiv`);
 
     newDiv.setAttribute(
-      'style', `
-      width: ${containerDimensions / divsPerSide + 'px'}; 
-      height: ${containerDimensions / divsPerSide + 'px'};
+      "style",
+      `
+      width: ${containerDimensions / divsPerSide + "px"}; 
+      height: ${containerDimensions / divsPerSide + "px"};
       background-color: rgba(0,0,0, 0.5);
-      `)
+      `
+    );
     con.appendChild(newDiv);
   }
-  smallDivs = document.querySelectorAll('.smallDiv');
+  smallDivs = document.querySelectorAll(".smallDiv");
   addEventListeners(smallDivs, resetColorsBtn);
 }
 
 createDiv(side, container);
-rangeValue.textContent = `Squares per side: ${rangeInput.value}`
+rangeValue.textContent = `Squares per side: ${rangeInput.value}`;
 
-rangeInput.oninput = function() {
+rangeInput.oninput = function () {
   side = this.value;
-  document.querySelector('.container').remove();
+  document.querySelector(".container").remove();
 
-  const newContainer = document.createElement('div');
-  newContainer.classList.add('container');
-  document.body.insertAdjacentElement('afterbegin', newContainer);
+  const newContainer = document.createElement("div");
+  newContainer.classList.add("container");
+  rangeContainer.insertAdjacentHTML("afterend", newContainer);
 
   createDiv(side, newContainer);
   addEventListeners(smallDivs, resetColorsBtn);
   rangeValue.textContent = `Squares per side: ${this.value}`;
-}
-
-/*
-Depreceted button meant for changing divs per side
-changeSizeBtn.addEventListener('click', () => {
-  do {
-    side = Number(prompt(`How many squares per side do you want your box to have? (it has to be fewer than 100) `));
-  } while(side > 100 || side <= 0 || Number(side) !== side);
-  document.querySelector('.container').remove();
-
-  const newContainer = document.createElement('div');
-  newContainer.classList.add('container');
-  document.body.insertAdjacentElement('afterbegin', newContainer);
-
-  createDiv(side, newContainer);
-  addEventListeners(smallDivs, resetColorsBtn);
-})
+};
 */
